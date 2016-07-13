b34ea59a778f6779791a17dfeb928ee172298904
tvoronenko <tetiana.voronenko@gmail.com>
2016-07-13 01:15:08 -0700

    fix index in insertion algoritm and add example

--- Cracking_coding_interview_Python/Other/insertion.py
+++ Cracking_coding_interview_Python/Other/insertion.py
@@ -1,12 +1,16 @@
-import sys
 
 def exchange(a, i, j):
     a[i], a[j] = a[j], a[i]
     
-def sort(a):
+def insertion_sort(a):
     n = len(a)
     for i in range(1, n):
         j = i
         while (j > 0) and (a[j] < a[j-1]):
-            exchange(a, i, j-1)
+            exchange(a, j, j-1)
             j -= 1
+
+
+array_int = [54,26,93,17,77,31,44,55,20]
+insertion_sort(array_int)
+print(array_int)
