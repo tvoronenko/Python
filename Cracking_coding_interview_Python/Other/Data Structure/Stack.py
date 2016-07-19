727a4c0bd6b42b18c522bfc8f52c08209ee6c229
tvoronenko <tetiana.voronenko@gmail.com>
2016-07-19 00:11:34 -0700

    add exception to stacj implementation

--- Cracking_coding_interview_Python/Other/Data Structure/Stack.py
+++ Cracking_coding_interview_Python/Other/Data Structure/Stack.py
@@ -1,18 +1,26 @@
 class Stack:
-     def __init__(self):
-         self.items = []
+    def __init__(self):
+        self.items = []
 
-     def isEmpty(self):
-         return self.items == []
+    def isEmpty(self):
+        return self.items == []
 
-     def push(self, item):
-         self.items.append(item)
+    def push(self, item):
+        self.items.append(item)
 
-     def pop(self):
-         return self.items.pop()
+    def pop(self):
+        if self.is_empty():
+            raise StackEmptyException
+        else:
+            return self.items.pop()
 
-     def peek(self):
-         return self.items[len(self.items)-1]
+    def peek(self):
+        if self.is_empty():
+            raise StackEmptyException
+        else:
+            return self.items[-1]
 
-     def size(self):
-         return len(self.items)
+    def size(self):
+        return len(self.items)
+class StackEmptyException(Exception):
+    pass
