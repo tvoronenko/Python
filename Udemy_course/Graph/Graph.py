51151700a6b9e641c799e4f60a788ccf0b45c0a8
tvoronenko <tetiana.voronenko@gmail.com>
2017-05-13 21:13:40 -0700

    Add BST WordLadderGraph

--- Udemy_course/Graph/Graph.py
+++ Udemy_course/Graph/Graph.py
@@ -54,7 +54,7 @@ class Graph:
 g = Graph()
 for i in range(6):
     g.addVertex(i)
-g.addEdge(0,1,5)
+g.addEdge(7,1,5)
 g.addEdge(0,5,2)
 g.addEdge(1,2,4)
 g.addEdge(2,3,9)
@@ -63,6 +63,6 @@ g.addEdge(3,5,3)
 g.addEdge(4,0,1)
 g.addEdge(5,4,8)
 g.addEdge(5,2,1)
-for v in g:
-    for w in v.getConnections():
-        print("( %s , %s )" % (v.getId(), w.getId()))
+#for v in g:
+ #   for w in v.getConnections():
+    #    print("( %s , %s )" % (v.getId(), w.getId()))
