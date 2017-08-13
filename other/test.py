3c60c2b3c46022f4e2e45b40ca0f66fe639aa0af
tvoronenko <tetiana.voronenko@gmail.com>
2017-08-12 21:30:38 -0700

    Add files via upload

--- other/test.py
+++ other/test.py
@@ -1,72 +1,41 @@
+# def  isSubsetSum(set, n, sum):
+#         subset= [[True for x in range(n+1)] for x in range(sum+1)]
+#       
+#         for i in range(n+1):
+#             subset[0][i] = True
+#       
+#         for i in range(sum+1):
+#             subset[i][0] = False
+#             
+#         for i in range(sum+1):
+#             for j in range(n+1):
+#                 subset[i][j] = subset[i][j-1]
+#                 if (i >= set[j-1]):
+#                     subset[i][j] = subset[i][j] or  subset[i - set[j-1]][j-1]
+#         for i in range(sum+1):
+#             for j in range(n+1):
+#                 print(subset[i][j])
+#             print("\n")
+# 
+#         return subset[sum][n]
+#     
+# print(isSubsetSum([70,30,33,23,4,4,34,95,50,10,10,7],12,108))
 from collections import defaultdict
 from collections import deque
-from docutils.parsers.rst.directives import path
+import math
 class Graph:
     def __init__(self):
-        self.vertex = defaultdict(list)
-        self.nodes =set()
+        self.edge = defaultdict(list)
+        self.nodes = {}
+        self.cache_path = defaultdict(list)
+        self.cache_subtree = defaultdict(list)
         
-    def add_edge(self,start,end=[]):
-        self.nodes.add(start)
-        if end != []:
-            self.nodes.add(end)
-            self.vertex[end].append(start)
-            self.vertex[start].append(end)
-        else:
-           self.vertex[start] = end
-        #self.vertex[end].append(start)
-        
-    def generate_edge(self,dict_edge):
-        if dict_edge != None:
-            for node in dict_edge:
-                for nebr in dict_edge[node]:
-                    self.add_edge(node, nebr)
-                    
-    def bfs(self,start = None):
-        visited,to_explore = set(),deque()
-        path_tree={}
-        travers_list = []
-        for start in (self.vertex):
-           if start not in visited:
-                to_explore.append(start)
-                visited.add(start)
-                travers_list.append(start)
-                while to_explore:
-                    next_node = to_explore.popleft()
-                    for nxt in self.vertex[next_node]:
-                        if nxt not in visited and nxt!=None:
-                            visited.add(nxt)
-                            travers_list.append(nxt)
-                            path_tree[nxt] = next_node
-                            to_explore.append(nxt)
-        print path_tree
-        return travers_list
+    def add_edge(self,start,end):
+        self.edge[start].append(end)
+        self.edge[end].append(start)
     
-    def find_isolated_nodes(self):
-        """ returns a list of isolated nodes. """
-        isolated = []
-        for node in self.vertex:
-            if not self.vertex[node]:
-                isolated += node
-        return isolated
-    def __str__(self):
-        res = "Graph:  "
-        for node in self.vertex:
-            for nebr in self.vertex[node]:
-                res += "({}, {})".format(node,nebr)
-        return res
-    
-    def bfs_paths(self,start,end):
-        to_explore = deque()
-        to_explore.append((start,[start]))
-        while to_explore:
-            (next_node, path) = to_explore.popleft()
-            for nxt in self.vertex[next_node]:
-                if nxt not in path:
-                    if nxt == end:
-                        yield path + [nxt]
-                    else:
-                        to_explore.append((nxt,path + [nxt]))
+    def add_node(self,arr):
+        self.nodes = {(i+1):v for i,v in enumerate(arr)}
     
     def short_path_bfs(self,start,end):
         visited, to_explore = set(),deque()
@@ -77,88 +46,16 @@ class Graph:
             next_node = to_explore.popleft()
             if next_node == end:
                 break
-            for nxt in self.vertex[next_node]:
+            for nxt in self.edge[next_node]:
                 if nxt not in visited:
                     visited.add(nxt)
                     parent[nxt] = next_node
                     to_explore.append(nxt)            
         
-        #reconstruct_path
-        path = []
-        if (end not in visited) or len(parent)==0:
-            return path
-        else:
-            path.append(end)
-            current =  parent[end]
-            while start != current:
-                path.append(current)
-                current = parent[current]
-            path.append(start)
-            
-        return path
-    def longest_path(self):
-        max_diam = 0
-        last_vertix = ''
-        path = self.bfs()
-        if path != None:
-            i=len(path)-1
-            while i>=0 and path[i] in self.find_isolated_nodes(): 
-                i-=1
-            last_vertix = path[i]
-        for x in self.vertex:
-            for path in self.bfs_paths(last_vertix, x):
-                max_diam = max(max_diam, len(path))
-#         print last_vertix
-#         if max_path !=0:        
-#             for path in self.bfs(last_vertix):
-#                     print path
-#                     if max_diam < len(path):
-#                         max_diam = len(path)
-#             paths = list(self.bfs_paths(x))
-#             for y in self.vertex:
-#                 if x!=y and x not in self.find_isolated_nodes() and y not in self.find_isolated_nodes():
-#                     path = self.short_path_bfs(x,y)
-#                     print str(path)+str(x)+str(y)
-#                     max_diam = max(max_diam,len(path)-1)
-        print max_diam
-        
-    def diameter(self):
-        print "diameter"
-        max_diam = 0
-        last_vertix = ''
-        path = self.bfs()
-        paths = []
-        if path != None:
-            i=len(path)-1
-            while i>=0 and path[i] in self.find_isolated_nodes(): 
-                i-=1
-            last_vertix = path[i]
-        for x in self.vertex:
-            if x!=last_vertix and x not in self.find_isolated_nodes():
-                path = self.short_path_bfs(last_vertix, x)
-                print str(path)+str(last_vertix)+str(x)
-                if path != None and len(path)!=0:
-                    max_diam = max(max_diam, len(path))
-        return max_diam
-#                     
-#         
-    def short_path_dfs(self,start,end):
-        visited,to_explore = set(), [start]
-        visited.add(start)
-        parent={}
-        while to_explore:
-            next_node = to_explore.pop()
-            if next_node == end:
-                break
-            for nxt in self.vertex[next_node]:
-                if nxt not in visited:
-                    visited.add(nxt)
-                    parent[nxt] = next_node
-                    to_explore.append(nxt)
         # reconstruct_path
         path = []
         if end not in visited:
-            return path
+            return None
         else:
             path.append(end)
             current =  parent[end]
@@ -166,257 +63,63 @@ class Graph:
                 path.append(current)
                 current = parent[current]
             path.append(start)
+        name_path="{}:{}".format(start,end)
+        self.cache_path[name_path]=path
         return path
-        
-        
-    def dfs_paths(self,start,end):
-        to_explore = [(start,[start])]
+
+    def divide(self,param):
+        name_path="{}:{}".format(start,end)
+        if name_path in self.cache_path:
+            path = self.cache_path[name_path]
+        else:
+            path = self.short_path_bfs(param[0],param[1])
+        for i in path:
+            self.nodes[i]=math.ceil(self.nodes[i]/param[2])
+
+    def get_subtree(self,node):
+        result=[]
+        result.append(node)
+        to_explore = [node]
         while to_explore:
-            (next_node, path) = to_explore.pop()
-            for nxt in self.vertex[next_node]:
-                if nxt not in path:
-                    if nxt == end:
-                        yield path + [nxt]
-                    else:
-                        to_explore.append((nxt,path + [nxt]))
-                    
+            current = to_explore.pop()
+            for nxt in self.edge[node]:
+                if nxt>node:
+                    result.append(nxt)
+                    to_explore.append(nxt)
         
-    def dfs(self,start=None):
-        visited,to_explore = set(),[start]
-        path_tree={}
-        travers_list = []
-        travers_list.append(start)
-        visited.add(start)
-        for start in (self.vertex):
-            if start not in visited:
-                while to_explore:
-                     next_node = to_explore.pop()
-                     for nxt in self.vertex[next_node]:
-                         if nxt not in visited:
-                             travers_list.append(nxt)
-                             visited.add(nxt)
-                             path_tree[nxt] = next_node
-                             to_explore.append(nxt)
-        print path_tree
-        return travers_list
-    #reach separate vertex                    
-    def dfs_recurvive(self,start):
-       visited = set()
-       for i in (self.nodes):
-           if i not in visited:
-               self._dfs_recurvive(i,visited)
+        self.cache_subtree[node]=result
+        return result
 
-    
-    def _dfs_recurvive(self,start,visited):
-        visited.add(start)
-        print start
-        for nxt in self.vertex[start]:
-                if nxt not in visited:
-                    self._dfs_recurvive(nxt,visited)
-    #direct graph
-    def has_cycle_rec_direct(self):
-        visited = set()
-        onStack = set()
-        for i in self.nodes:
-            if i not in visited:
-                if self._has_cycle_direct(i,i,visited,onStack) == True:
-                    return True
-                onStack.remove(i)
-        return False
-                
-    def _has_cycle_direct(self,start,end,visited,onStack):
-        visited.add(start)
-        onStack.add(start)
-        for nxt in self.vertex[start]:
-            if nxt not in visited:
-                if (nxt not in visited) and(self._has_cycle_direct(nxt,start,visited,onStack) == True):
-                    return True
-            else:
-                if nxt in onStack:
-                    return True
-        return False
-    
-    #undirect version O(V+E) 
-    def has_cycle_rec(self):
-        visited = set()
-        for i in self.nodes:
-            if i not in visited:
-                if self._has_cycle(i,i,visited) == True:    
-                    return True
-        return False
-                
-    def _has_cycle(self,start,end,visited):
-        visited.add(start)
-        for nxt in self.vertex[start]:
-            if nxt not in visited:
-                if self._has_cycle(nxt,start,visited) == True:
-                    return True
-            else:
-                if nxt != end:
-                    return True
-        return False
-#undirect version O(V+E) #not working
-    def has_cycle(self):
-        visited = set()
-        to_explore = []
-        for i in self.vertex:
-            if i not in visited:
-                to_explore = [(i,i)]
-                while to_explore:
-                    (node_next,end_node) = to_explore.pop()
-                    visited.add(node_next)
-                    for nxt in self.vertex[node_next]:
-                        if nxt not in visited:
-                            to_explore.append((nxt,node_next))
-                        else:
-                            if nxt != end_node:
-                                return True 
-        return False
+    def query(self,param):
+        if param[0] in self.cache_subtree:
+            subtree = self.cache_subtree[param[0]]
+        else:
+            subtree = self.get_subtree(param[0])
+        max_xor = 0
+        for i in subtree:
+            max_xor = max(max_xor,(self.nodes[i]^param[1]))
+        print(max_xor)
 
-class Graph_matrix:
-    def __init__(self, V):
-        self.vertex=['' for x in range(V)]
-        self.size = 0
-        self.adj_matrix=[[0 for x in range(V)] for x in range(V) ]
-    
-    def get_index(self,node_name):
-        if node_name in  self.vertex:
-            return self.vertex.index(node_name)
-        
-    def add_edge(self,start,end):
-       if start not in  self.vertex:
-           self.vertex[self.size] = start
-           self.size +=1
-       if end not in  self.vertex:
-           self.vertex[self.size] = end
-           self.size +=1
-       self.adj_matrix[self.get_index(start)][self.get_index(end)] = 1
-       self.adj_matrix[self.get_index(end)][self.get_index(start)] = 1
-    
-    def get_neibor(self,node):
-        index_node = self.get_index(node)
-        for index_neib,x in  enumerate(self.adj_matrix[index_node]):
-            if x==1:
-                node_neib = self.vertex[index_neib]
-                yield  node_neib
-    
-    def bfs(self,start):
-        visited, to_explore = set(),deque()
-        for start in (self.vertex):
-           if start not in visited:
-                to_explore.append(start)
-                visited.add(start) 
-                while to_explore:
-                    cur_node = to_explore.popleft()
-                    visited.add(cur_node)
-                    print cur_node
-                    for nxt in self.get_neibor(cur_node):
-                        if nxt not in visited:
-                            to_explore.append(nxt)
-                            visited.add(nxt)
-        
+    def multiply(self, param):
+        self.nodes[param[0]] = self.nodes[param[0]]*param[1] % 1000 + 9
 
-graph = { "a" : ["c"],
-          "b" : ["c", "e"],
-          "c" : ["a", "b", "d", "e"],
-          "d" : ["c"],
-          "e" : ["c", "b"],
-          "f" : []
-        }       
-g = Graph()
-# g.generate_edge(graph)
-g.add_edge('A','B')
-g.add_edge('A','S')
-g.add_edge('S','G')
-g.add_edge('S','C')
-g.add_edge('G','D')
-g.add_edge('G','E')
-g.add_edge('G','F')
-g.add_edge('C','F')
-g.add_edge('C','H')
-g.add_edge('F','E')
-g.add_edge('K')
-# print(str(g))
-#print(g.bfs('C'))
-print(g.dfs('B'))
-# print(g.longest_path())
-# print(g.diameter())
-# print(g.find_isolated_nodes())
-# print(g.short_path_bfs('A', 'H'))
-# g.diameter()
-# g1 = Graph_matrix(10)
-# g1.add_edge('A','B')
-# g1.add_edge('A','S')
-# g1.add_edge('S','G')
-# g1.add_edge('S','C')
-# g1.add_edge('G','D')
-# g1.add_edge('G','E')
-# g1.add_edge('G','F')
-# g1.add_edge('C','F')
-# g1.add_edge('C','H')
-# g1.add_edge('F','E')
-# print(g1.bfs('A'))
+g=Graph()
+n, k = input().strip().split(' ')
+n, k = [int(n), int(k)]
+vertex = list(map(int, input().strip().split(' ')))
+g=Graph()
+g.add_node(vertex)
+for a1 in range(n-1):
+        start, end = input().strip().split(' ')
+        start, end = [int(start), int(end)]
+        g.add_edge(start, end)
 
-# # g.add_edge('A','B')
-# g.add_edge('A','S')
-# g.add_edge('S','G')
-# g.add_edge('S','C')
-# g.add_edge('G','D')
-# g.add_edge('G','E')
-# #g.add_edge('F','G')
-# g.add_edge('C','F')
-# g.add_edge('C','H')
-# g.add_edge('H','C')
-# #g.add_edge('H','H')
-# #g.add_edge('E','F')
-# g.add_edge('K','L')
-
-# g.add_edge('A','B')
-# g.add_edge('A','S')
-# g.add_edge('S','G')
-# g.add_edge('S','C')
-# g.add_edge('G','D')
-# g.add_edge('G','E')
-# g.add_edge('G','F')
-# g.add_edge('C','F')
-# g.add_edge('C','H')
-# g.add_edge('F','E')
-# g.add_edge('K','L')
-# list=[]
-# max_len=0
-# for v in g.nodes:
-#     
-#     max_len=max(max_len,len(g.short_path_bfs('A',v)))
-# print(max_len)
-    
-# print("BFS")
-# g.bfs("A")
-# print("DFS")
-# g.dfs("A")
-# print("DFS recursive")
-# g.dfs_recurvive("A")
-# print(list(g.bfs_paths('A','E')))
-# print(list(g.dfs_paths('A','E')))
-#print(g.short_path_bfs('A', 'E'))
-# print(g.short_path_dfs('A', 'E'))
-# print(g.has_cycle_rec_direct())
-#print(g.has_cycle())
-# g.add_edge(1,2)
-# g.add_edge(1,3)
-# g.add_edge(3,4)
-# g.add_edge(3,5)
-# g.add_edge(4,6)
-# g.add_edge(4,7)
-# g.add_edge(4,8)
-# g.add_edge(5,8)
-# g.add_edge(5,9)
-# g.add_edge(9,7)
-# g.add_edge(8,7)
-# print(g.BFS(1))
-
-# g.addEdge(0, 1)
-# g.addEdge(0, 2)
-# g.addEdge(1, 2)
-# g.addEdge(2, 0)
-# g.addEdge(2, 3)
-# g.addEdge(3, 3)
+for a1 in range(k):
+        op = list(map(str, input().strip().split(' ')))
+        param = list(map(int,op[1:]))
+        if op[0]=="Divide":
+            g.divide(param)
+        elif op[0]=="Query":
+            g.query(param)
+        elif op[0]=="Multiply":
+            g.multiply(param)
